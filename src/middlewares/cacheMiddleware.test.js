0864891a87a2a2ca2924cf601f4afbf39f4af725
samia-dhahri <dhahrisamia@gmail.com>
2026-01-30 15:44:06 +0100

    Test cacheGet middleware

--- src/middlewares/cacheMiddleware.test.js
+++ src/middlewares/cacheMiddleware.test.js
@@ -7,3 +7,77 @@ jest.unstable_mockModule("../cache/cacheService.js", () =>({
 
 const { cacheGet, cacheSet } = await import("../cache/cacheService.js");
 const { cacheMiddleware } = await import("./cacheMiddleware.js");
+
+function createRes(){
+    const res = {};
+    res.headers = {};
+    res.statusCode = 200;
+
+    res.setHeader = jest.fn((k, v) => {
+        res.headers[k] = v;
+    });
+
+    res.status = jest.fn((code) => {
+        res.statusCode = code;
+        return res;
+    });
+
+    res.json = jest.fn((body) => body);
+    return res;
+}
+
+describe("cacheMiddleware", () => {
+    beforeEach(() => {
+        jest.clearAllMocks();
+        jest.spyOn(console, "error").mockImplementation(()=>{});
+        jest.spyOn(console, "log").mockImplementation(()=>{});
+    });
+
+    afterEach(() =>{
+        console.error.mockRestore();
+        console.log.mockRestore();
+    });
+
+    it("BYPASS cache x-cache-bypass=1", async () =>{
+        const mw = cacheMiddleware({keyFn: () => "k"});
+        const req = {headers: {"x-cache-bypass": "1"}};
+        const res = createRes();
+        const next = jest.fn();
+
+        await mw(req, res, next);
+        expect(res.setHeader).toHaveBeenCalledWith("X-Cache", "BYPASS");
+        expect(cacheGet).not.toHaveBeenCalled();
+        expect(next).toHaveBeenCalledTimes(1);
+    });
+
+    it("call next when keyFen returns false", async () =>{
+        const mw = cacheMiddleware({keyFn: () => null});
+        const req = {headers: {}};
+        const res = createRes();
+        const next = jest.fn();
+
+        await mw(req, res, next);
+        expect(cacheGet).not.toHaveBeenCalled();
+        expect(next).toHaveBeenCalledTimes(1);
+    });
+
+    it("should return HIT when cacheGet returns value", async () => {
+        const mw = cacheMiddleware({ keyFn: () => "product:1" });
+        const req = { headers: {} };
+        const res = createRes();
+        const next = jest.fn();
+
+        const cachedValue = { id: 1, name: "A" };
+        cacheGet.mockResolvedValue(cachedValue);
+
+        await mw(req, res, next);
+
+        expect(cacheGet).toHaveBeenCalledWith("product:1");
+        expect(res.setHeader).toHaveBeenCalledWith("X-Cache", "HIT");
+        expect(res.status).toHaveBeenCalledWith(200);
+        expect(res.json).toHaveBeenCalledWith(cachedValue);
+
+        // On doit avoir répondu, donc pas de next()
+        expect(next).not.toHaveBeenCalled();
+    });
+});
